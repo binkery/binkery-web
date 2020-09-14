d963b1ca9b477be7346a874b3f08ba8d29cc3d50
binkery@E30 <binkery@gmail.com>
2020-09-15 07:45:01 +0800

    update friend link

--- python/run.py
+++ python/run.py
@@ -167,6 +167,10 @@ def write_article_to_file(article):
                         <li><a href="http://94duo.com/" target="_blank">火币网</a></li>
                     	<li><a href="http://99ppc.com/tag/zhongbijiaoyisuoguanwang/" target="_blank">火币网</a></li>
                         <li><a href="https://www.okex.me/academy/zh/article/master-9" target="_blank">区块链是什么意思</a></li>
+                        <!--2020.09.15 51link.com-->
+                        <li><a href="http://qjlgb.com/dianshiju/" target="_blank">热播电视剧</a></li>
+                        <!--2020.09.15 51link.com-->
+                        <li><a href="http://www.nmgredcross.org.cn/story/" target="_blank">剧情吧</a></li>                        
                     </ul>
                     </div>
                 </div>
