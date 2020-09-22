89e8a02faa2fdaaade7974e3c537a93520ca8759
binkery <binkery@gmail.com>
2020-09-22 16:21:49 +0800

    update

--- python/run.py
+++ python/run.py
@@ -114,17 +114,7 @@ def write_article_to_file(article):
                     </blockquote>
                 </article>
                 <article>
-                    <script type="text/javascript">
-                        document.write('<a style="display:none!important" id="tanx-a-mm_103116298_40854605_110857550142"></a>');
-                        tanx_s = document.createElement("script");
-                        tanx_s.type = "text/javascript";
-                        tanx_s.charset = "gbk";
-                        tanx_s.id = "tanx-s-mm_103116298_40854605_110857550142";
-                        tanx_s.async = true;
-                        tanx_s.src = "//p.tanx.com/ex?i=mm_103116298_40854605_110857550142";
-                        tanx_h = document.getElementsByTagName("head")[0];
-                        if(tanx_h)tanx_h.insertBefore(tanx_s,tanx_h.firstChild);
-                    </script>
+                    
                 </article>
                 <!--
                 <div id="gitalk-container"></div>
