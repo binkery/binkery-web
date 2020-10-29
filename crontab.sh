017cf5f0df71c22e055c92e3e5cf66b15efdd083
binkery <binkery@gmail.com>
2020-10-29 17:26:50 +0800

    update

--- crontab.sh
+++ crontab.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-cd /usr/share/nginx/binkery-web/
+cd /root/html/binkery-web/
 COUNT=$(git pull | grep "Already up-to-date" | grep -v "grep" | wc -l)
 if [ $COUNT -eq 1 ]; then
     exit 0
 fi
-cd /usr/share/nginx/binkery-web/python/
+cd /root/html/binkery-web/python/
 python3 run.py
