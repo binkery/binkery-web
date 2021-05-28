c5bef90b3c38c15ef6fae66bb4177a4cc20750c2
binkery <binkery@gmail.com>
2021-05-28 18:04:41 +0800

    update

--- python/run.py
+++ python/run.py
@@ -155,6 +155,8 @@ def write_article_to_file(article):
                         <li><a href="https://www.xiaoz.me/" target="_blank">小z博客</a></li>
                         <li><a href="https://www.oldpan.me/" target="_blank">Oldpan的个人博客</a></li>
                         <li><a href="https://www.oldking.net/" target="_blank">老鬼的博客</a></li>
+                        <li><a href="https://www.okexo.com/" target="_blank">okex</a></li>
+                        
                     </ul>
                     </div>
                 </div>
